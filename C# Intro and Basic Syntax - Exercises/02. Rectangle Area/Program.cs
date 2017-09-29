89ec418daffb27e8d69fd3f15befdd851b6ad79b
MichelleMihova <michellemihova@gmail.com>
2017-09-30 02:43:36 +0300

    C# Intro and Basic Syntax- Exercises-solutions

---
+++ C# Intro and Basic Syntax - Exercises/02. Rectangle Area/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float widht =float.Parse(Console.ReadLine());
+			float height = float.Parse(Console.ReadLine());
+			float sum;
+			sum = widht * height;
+			Console.WriteLine("{0:F2}",sum);
+		}
+	}
+}
---
+++ C# Intro and Basic Syntax - Exercises/04. Beverage Labels/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name=Console.ReadLine();
+			double  volume = double  .Parse(Console.ReadLine ());
+			double  energy = double .Parse(Console.ReadLine());
+			double  sugar = double .Parse(Console.ReadLine());
+			double  ckal;
+			ckal = volume * (energy /100);
+			double sugars;
+			sugars = volume * (sugar  / 100);
+			Console.WriteLine("{0}ml {1}:", volume ,name);
+			Console.WriteLine("{0}kcal, {1}g sugars", ckal, sugars);
+
+		}
+	}
+}

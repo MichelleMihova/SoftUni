9a64940a83f00bbcfedbeb889e646c1e336d2f1d
MichelleMihova <michellemihova@gmail.com>
2017-09-30 03:19:07 +0300

    some exercise solutions

---
+++ C# Data Types and Variables - Exercises/03. Practice Chars And Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Practice_Chars_And_Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string First = "Software University";
+			char B = 'B';
+			char Y = 'y';
+			char E = 'e';
+			string second = "I love programming";
+			Console.WriteLine(First);
+			Console.WriteLine(B);
+			Console.WriteLine(Y);
+			Console.WriteLine(E);
+			Console.WriteLine(second);
+		}
+	}
+}

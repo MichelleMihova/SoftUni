f49eafa5f03133f87c773386c2ed6e79b748e49b
MichelleMihova <michellemihova@gmail.com>
2017-10-04 17:16:42 +0300

    ex 3

---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/03. Printing Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Printing_Triangle
+{
+	class Program
+	{
+		static void printingTriangle(int num)
+		{
+			for (int i = 1; i <= num; i++)
+			{
+				for (int j = 1; j <= i; j++)
+				{
+					Console.Write("{0} ",j);
+				}
+				Console.WriteLine();
+			}
+			for (int i = num-1; i >= 1; i--)
+			{
+				for (int j = 1; j <=i; j++)
+				{
+					Console.Write("{0} ", j);
+				}
+				Console.WriteLine();
+			}
+		}
+		static void Main(string[] args)
+		{
+			int num = int.Parse(Console.ReadLine());
+			printingTriangle(num);
+		}
+
+		
+	}
+}

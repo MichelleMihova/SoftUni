4d2b0fc16565457ff3450546e4603f18cfb85872
MichelleMihova <michellemihova@gmail.com>
2017-10-04 17:19:30 +0300

    ex 7

---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/07. Math Power/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Math_Power
+{
+	class Program
+	{
+		static double numberPower(double number, int power)
+		{
+			Double sum = 0;
+			sum=Math.Pow(number,power);
+			return sum;
+		}
+		static void Main(string[] args)
+		{
+			double number = double.Parse(Console.ReadLine());
+			int power = int.Parse(Console.ReadLine());
+			double sum = numberPower(number,power);
+			Console.WriteLine(sum);
+		}
+	}
+}

58169b7d932b1e939fed571a5c3b7aa48d0a626e
MichelleMihova <michellemihova@gmail.com>
2017-10-04 17:14:49 +0300

    first ex

---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/01. Blank Receipt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods___Lab
+{
+	class Program
+	{
+		static void printingTheHeader()
+		{
+			Console.WriteLine("CASH RECEIPT");
+			Console.WriteLine("------------------------------");
+		}
+		static void body()
+		{
+			Console.WriteLine("Charged to____________________");
+			Console.WriteLine("Received by___________________");
+		}
+		static void foother()
+		{
+			Console.WriteLine("------------------------------");
+			Console.WriteLine("\u00A9 SoftUni");
+		}
+		static void Main(string[] args)
+		{
+			printingTheHeader();
+			body();
+			foother();
+		}
+	}
+}

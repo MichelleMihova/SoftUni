37b840b3b85a2d3d2808e5f06e0abcaf36cb71f8
MichelleMihova <michellemihova@gmail.com>
2017-10-04 17:21:00 +0300

    methods -lab exercise

---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/09. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Multiply_Evens_by_Odds
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			int digits=number.ToString().Count();
+			int evenSum = 0;
+			int oddSum = 0;
+			for (int i = 1; i <= digits; i++)
+			{
+				int lastNum = 0;
+				lastNum = number % 10;
+				number = number / 10;
+				if (lastNum % 2 == 0)
+				{
+
+					evenSum = evenSum + lastNum;
+
+					
+				}
+				else
+				{
+					oddSum = oddSum + lastNum;
+
+				}
+			}
+			int sum = evenSum * oddSum;
+			Console.WriteLine(sum);
+		}
+	}
+}
---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/10. Holidays Between Two Dates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Holidays_Between_Two_Dates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+
+
+		}
+	}
+}

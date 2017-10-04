d07a347bcfcde865447158990cf3994d566b8afc
MichelleMihova <michellemihova@gmail.com>
2017-10-04 17:19:05 +0300

    ex 6

---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/06. Calculate Triangle Area/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Calculate_Triangle_Area
+{
+	class Program
+	{
+		static double triangleArea(double width, double height)
+		{
+			return width*height/2;
+		}
+		static void Main(string[] args)
+		{
+			double width = double.Parse(Console.ReadLine());
+			double height = double.Parse(Console.ReadLine());
+			double area = triangleArea(width, height);
+			Console.WriteLine(area);
+		}
+	}
+}

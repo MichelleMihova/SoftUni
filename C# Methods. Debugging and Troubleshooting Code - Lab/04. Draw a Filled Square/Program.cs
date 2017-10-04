b777ad726d80ef3164f1035d78cbfebb559f804a
MichelleMihova <michellemihova@gmail.com>
2017-10-04 17:18:09 +0300

    ex 4

---
+++ C# Methods. Debugging and Troubleshooting Code - Lab/04. Draw a Filled Square/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Draw_a_Filled_Square
+{
+	class Program
+	{
+		static void headerAndFother(int number)
+		{
+			Console.Write(new string('-',number*2));
+			Console.WriteLine();
+		}
+		static void body(int number)
+		{
+			for (int i = 1; i <= number-2; i++)
+			{
+				Console.Write('-');
+				for (int j = 1; j <= ((number * 2 - 2) / 2); j++)
+				{
+					Console.Write("\\/");
+				}
+				Console.Write('-');
+				Console.WriteLine();
+			}
+		}
+		static void Main(string[] args)
+		{
+			int number = int.Parse(Console.ReadLine());
+			headerAndFother(number);
+			body(number);
+			headerAndFother(number);
+		}
+	}
+}
